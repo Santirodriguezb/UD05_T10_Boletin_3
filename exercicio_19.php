83de7b163f4421b7fc94d8663d44f900aad69a92
Santirodriguezb <santiagorodriguezb@ieslaxeiro.com>
2022-02-17 13:36:55 +0100

    Exercicio 19

--- exercicio_19.php
+++ exercicio_19.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exercicios 16,17,18</title>
+    <title>Exercicio 19</title>
 </head>
 <body>
-    <form action="<?php $_SERVER['PHP_SELF']?>" method="post">
+<form action="<?php $_SERVER['PHP_SELF']?>" method="post">
     <label for="numero">Introduce un número</label>
     <input type="number" name="numero">
     <br>
@@ -20,13 +20,22 @@
             echo "<p>Tes que introducir un numero</p>";
         } else if (filter_var($_REQUEST['numero'],FILTER_VALIDATE_INT)){
             $meuNumero = $_REQUEST['numero'];
-            $meuNumero = abs($meuNumero);
-            $primeirodixito = substr($meuNumero,0,1);
-            echo "<p>O primeiro dixito do numero é: $primeirodixito . </p>" ;
-            $ultimodixito = substr($meuNumero,strlen($meuNumero),-1,1);
-            echo "<p>O ultimo dixito do numero é: $ultimodixito</p>";
-            $numerodixitos = strlen($meuNumero);
-            echo "<p>O numero total de dixitos é: $numerodixitos</p>";
+            $escapicua = true;
+
+            for ($i=0; $i<strlen($meuNumero); $i++) {
+                $esquerda=substr($meuNumero,$i,1);
+                $dereita=substr($meuNumero,strlen($meuNumero)-1-$i,1);
+                if($esquerda!=$dereita){
+                    $escapicua = false;
+                }
+            }
+
+            if ($escapicua) {
+                echo "<p>El numero introducido es capicua: $escapicua</p>";
+            } else {
+                
+            }
+
         } else {
             echo "<p>Tes que introducir un numero</p>";
         }
